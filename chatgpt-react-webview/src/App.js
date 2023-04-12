aac2979352ed54fbd79ae23e8ca1bf83c4fb4731
Ted <withwooyong@yanadoocorp.com>
2023-04-12 20:22:46 +0900

    20230412_Ted

---
+++ chatgpt-react-webview/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import ChatGPTWebview from "./components/ChatGPTWebview";
+
+function App() {
+  return (
+    <div>
+      <h1>My App</h1>
+      <ChatGPTWebview />
+    </div>
+  );
+}
+
+export default App;
